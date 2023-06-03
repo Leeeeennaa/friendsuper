04212d0c170460901d52faf4b618708b1b50cfa2
lena <leeeeeennaa@mail.ru>
2023-06-03 19:32:58 +0700

    Время

--- lab8.js
+++ lab8.js
@@ -41,4 +41,9 @@ function Del() {
     document.getElementsByClassName('d')[0].value="";
 }
 
-
+function showTime() {
+    let today = new Date();
+    let currentTime = today.toLocaleTimeString('ru-RU');
+    document.getElementById('time').innerHTML = currentTime;
+}
+setInterval(showTime,1000);
